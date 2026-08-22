610dbe2f70f14a2a32cdb1aa853ef71f7ff019c6
Shivani Verma <shivani.vr.verma@gmail.com>
2026-08-22 14:14:56 +0530

    fix: correct import statement for SongContextProvider in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { RouterProvider } from 'react-router-dom'
 import router from './routes/app.routes'
 import { AuthProvider } from './features/auth/state/auth.context'
 import { Toaster } from 'react-hot-toast';
-import { SongContenxtProvider } from './features/home/state/Song.context'
+import { SongContenxtProvider } from './features/home/state/Song.context';
 
 
 const App = () => {
