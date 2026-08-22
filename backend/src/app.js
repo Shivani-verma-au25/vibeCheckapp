3eb09cee9efad0d35438d05b44038e0923005245
Shivani Verma <shivani.vr.verma@gmail.com>
2026-08-23 01:06:26 +0530

    fix: remove fallback URL for baseURL in API services

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 app.use(Cors({
-  origin: configrations.frontendUrl || 'http://localhost:5173',
+  origin: configrations.frontendUrl,
   // origin: 'http://localhost:5173',
   credentials : true,
 }))
--- frontend/src/features/home/services/song.api.js
+++ frontend/src/features/home/services/song.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : import.meta.VITE_BACKEND_URL || "http://localhost:8000",
+    baseURL : import.meta.VITE_BACKEND_URL,
     withCredentials: true
 });
 
